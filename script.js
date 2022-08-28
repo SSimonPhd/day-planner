8324ba2752aad349691d1264f77ea0be6b6a5a39
Scott Simon <simon_phd@protonmail.com>
2022-08-28 01:11:50 -0500

    Debugged JS, added comments

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Moment declaration for day and time 
+// Date & time declaration
 moment(Date);
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
 // Moment Current time 
 var currentTime = moment();
-// Returns current time to the nearest hour - 12:30 becomes 12:00
+// Returns current time to nearest hour
 currentTime = currentTime.startOf("hour");
-// Calculates the start of day + 9 to return 9 am
+// Calculates start of day
 var beforeTime = moment().startOf('day').add(9, "hours");
 
 // Time blocks
@@ -13,55 +13,54 @@ var beforeTime = moment().startOf('day').add(9, "hours");
 var time1 = beforeTime.add(0, "h");
 // var time1 = beforeTime;
 time1 = time1.format('hh:mm A');
-// Populates time formula into html
+// Loads time format into html
 $(".block1").text(time1);
 // 10 AM
 var time2 = beforeTime.add(1, "h");
 time2 = time2.format('hh:mm A');
-// Populates time formula into html
+// Loads time format into html
 $(".block2").text(time2);
 // 11 AM
 var time3 = beforeTime.add(1, "h");
 time3 = time3.format('hh:mm A');
-// Populates time formula into html
+// Loads time format into html
 $(".block3").text(time3);
 // 12 PM
 var time4 = beforeTime.add(1, "h");
 time4 = time4.format('hh:mm A');
-// Populates time formula into html
+// Loads time format into html
 $(".block4").text(time4);
 // 1 PM
 var time5 = beforeTime.add(1, "h");
 time5 = time5.format('hh:mm A');
-// Populates time formula into html
+// Loads time format into html
 $(".block5").text(time5);
 // 2 PM
 var time6 = beforeTime.add(1, "h");
 time6 = time6.format('hh:mm A');
-// Populates time formula into html
+// Loads time format into html
 $(".block6").text(time6);
 // 3 PM
 var time7 = beforeTime.add(1, "h");
 time7 = time7.format('hh:mm A');
-// Populates time formula into html
+// Loads time format into html
 $(".block7").text(time7);
 // 4 PM
 var time8 = beforeTime.add(1, "h");
 time8 = time8.format('hh:mm A');
-// Populates time formula into html
+// Loads time format into html
 $(".block8").text(time8);
 // 5 PM
 var time9 = beforeTime.add(1, "h");
 time9 = time9.format('hh:mm A');
-// Populates time formula into html
+// Loads time format into html
 $(".block9").text(time9);
 
-// Function to compare hour slots with current time 
-// Update these to coordinating numbers
+// Function to compare time blocks with current time 
 function testTime() {
     // Add time1 9AM
     time1 = moment().startOf('day').add(9, "hours");
-    // Adjusts current time to the hour
+    // Adjusts current time to hour
     currentTime = currentTime.startOf("hour");
     // Add time1 if/else
     if (currentTime.isAfter(time1)) {
@@ -171,7 +170,7 @@ function testTime() {
     };
 }
 testTime();
-// Loops through input area to get item from local storage
+// Loops input to retreive item from local storage
 var x = [9, 10, 11, 12, 1, 2, 3, 4, 5];
 // Test loop:
 for (var i = 0; i < x.length; i++) {
@@ -179,7 +178,7 @@ for (var i = 0; i < x.length; i++) {
     // form - control
     $(".form" + x[i]).val(dataHour);
 }
-// Event listener to save to local stroage
+// Event listener save to local storage
 $(".saveBtn").click(function () {
     event.preventDefault();
     var formValue = $(this).siblings(".form-control").val();
